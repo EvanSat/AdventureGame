f3115c83ad3d291271dab0605aa8225601b0a3f7
Alison Major <alison.major@gmail.com>
2021-02-21 21:44:26 -0500

    Handle missing scene
    If a scene can't be found using the given ID, re-display the previous scene with a message that there was a problem.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from scenes.handle import print_scene
 from scenes.nodes import get_scene_from_story, get_story_links
 from stories.developer_adventure import story
 
-def handle_scene(choice, current_scene):
+def handle_scene(choice, current_scene, previous_scene):
     if choice == -1:
         # Print start scene
         # TODO: print the start screen
@@ -13,12 +13,17 @@ def handle_scene(choice, current_scene):
         options = get_story_links(current_scene)
         option_id = options[choice_index]["id"]
         current_scene = get_scene_from_story(story, option_id)
+        if current_scene == None:
+            current_scene = previous_scene
 
     # Clear the terminal screen
     os.system('cls' if os.name == 'nt' else 'clear')
 
     # TODO: handle and store new inventory
     # TODO: handle and store point adjustments
+
+    if previous_scene == current_scene:
+        print("There was an error fetching next scene. Try again.\n")
 
     print_scene(current_scene)
 
@@ -27,11 +32,12 @@ def handle_scene(choice, current_scene):
     except ValueError:
         pass
 
-    handle_scene(selection, current_scene)
+    previous_scene = current_scene
+    handle_scene(selection, current_scene, previous_scene)
 
 
 def main():
-    handle_scene(-1, story[0])
+    handle_scene(-1, story[0], story[0])
 
 if __name__ == '__main__':
     main()
