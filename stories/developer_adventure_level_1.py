0da96b2e63bac4ba7c95dff098d1f1d8860652d2
EvanSat <53355852+EvanSat@users.noreply.github.com>
2021-02-24 15:06:09 -0600

    Adding Level 1
    Commit for Level 1 into the refactor branch

--- stories/developer_adventure_level_1.py
+++ stories/developer_adventure_level_1.py
@@ -1,12 +1,21 @@
 level_1 = [
     {
         "id": "Level1_SceneA",
-        "text": "Level 1 text\nthis is the story text",
-        "point_adjust": 105,
-        "new_inventory": "Friend Ethan",
+        "text": "[[cyan]]You are a senior in a high school with interest to create new things. You live \n"
+                "with your parents and have a dog named Mai lo. Your best friend Katie is in the same class with \n"
+                "you, she likes music and plans to become producer. You have an uncle who is a pen tester and you \n"
+                "think he is cool. You still have 3 months to figure out what you want to do. You have a literature \n"
+                "test tomorrow. You have the basic knowledge and you are prepared as any other student in class. It's\n"
+                "an early night and it's getting boring. What do you want to do? [[resetcolor]]",
+        #"point_adjust": 105,
+        #"new_inventory": "Friend Ethan",
         "links": [
-            {"label":"Option B", "id":"Level2_SceneA"},
-            {"label":"Option C", "id":"Level3_SceneA"}
+            {"label": "Prepare for tomorrow's literature test", "id": "Level5_SceneA"},
+            {"label": "Go to sleep", "id": "Level5_SceneB"},
+            {"label": "Check social media", "id": "Level2_SceneA"},
+            {"label": "Play World of Warcraft with friends", "id": "Level2_SceneB"},
+
+
         ]
     }
 ]
